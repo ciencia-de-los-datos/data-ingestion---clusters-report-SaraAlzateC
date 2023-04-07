45d8611db770241205da8c29de06292156cf9621
SaraAlzateC <116410244+SaraAlzateC@users.noreply.github.com>
2023-04-06 19:46:24 -0500

    intento 2

--- pregunta.py
+++ pregunta.py
@@ -13,10 +13,10 @@ import pandas as pd
 
 
 def ingest_data():
-
     df = pd.read_csv("clusters_report.txt")
     df = df.rename(columns=lambda x: x.lower().replace(" ", "_"))
-    df["principales_palabras_clave"] = df["principales_palabras_clave"].str.replace(",\s+", ", ")
+    df["principales_palabras_clave"] = df["principales_palabras_clave"].str.replace("\s+"," ").str.replace(",\s+", ", ").str.strip().str.rstrip(".")
     df["porcentaje_de_palabras_clave"] = df["porcentaje_de_palabras_clave"].str.replace("%", "").str.replace(",", ".")
     df["porcentaje_de_palabras_clave"] = df["porcentaje_de_palabras_clave"].astype(float)
+    df["cantidad_de_palabras_clave"] = df["cantidad_de_palabras_clave"].astype(int)
     return df
